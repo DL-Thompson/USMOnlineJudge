e4981efe09a4ed5a0cdfd40f9961cc8753e6979e
Lucas <Daniel.L.Thompson@eagles.usm.edu>
2014-02-01 16:13:06 -0600

    Added the basic website template with html files for testing.

--- judge/judge/views.py
+++ judge/judge/views.py
@@ -7,5 +7,22 @@ def hello(name=None):
 	return render_template('hello.html', name=name)
 	
 @app.route('/')
-def front_page():
-	return 'USM Online Judge main page'
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/exercises')
+def exercises():
+    return render_template("exercises.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/statistics')
+def statistics():
+    return render_template("statistics.html")
