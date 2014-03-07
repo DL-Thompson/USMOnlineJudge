9bd3d151ea3b171762c3b6f68fbc0f62b72a562d
lucas <Daniel.L.Thompson@eagles.usm.edu>
2014-03-07 11:56:55 -0600

    Removed a duplicated callback that I missed in previous fix.

--- judge/judge/static/codemirror-ui.js
+++ judge/judge/static/codemirror-ui.js
@@ -74,7 +74,6 @@ CodeMirrorUI.prototype = {
     if (mirrorOptions.onChange) {
         mirrorOptions.oldOnChange = mirrorOptions.onChange;
         mirrorOptions.onChange = function(instance, textChanged) {
-            mirrorOptions.oldOnChange();
             onChange();
             //passes the original codemirror onChange variables back to the function
             //declared in the codemirror options section of codemirror-ui instantiation
